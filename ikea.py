b5d8ea3d0c20f8017e356ee06f2510538136f323
pranavs28 <47789665+pranavs28@users.noreply.github.com>
2020-05-25 22:13:54 -0400

    Update ikea.py

--- ikea.py
+++ ikea.py
@@ -1,24 +1,24 @@
 from selenium import webdriver
 import bs4, smtplib
 
+# open Firefox browser, load webpage, extract html, close browser
 driver = webdriver.Firefox()
-
 driver.get('https://ikea-status.dong.st')
-
 html = driver.page_source
-
 driver.close()
 
+# open navigable html directory
 soup = bs4.BeautifulSoup(html, 'html.parser')
 
+# Find IKEA West Chester by going to state (OH) and then picking the second location (statuscode[1])
 statuscode = soup.find("div", class_ ="OH")
-
 statuscode = statuscode.ul.find_all("li")
 statuscode = str(statuscode[1])
 
-print(statuscode)
-
+# Create table with alert recipients
 toAddress = ['email1@gmail.com', 'email2@gmail.com']
+
+# Send email alerts
 if 'Open' in statuscode:
     conn = smtplib.SMTP('smtp.gmail.com', 587)
     conn.ehlo()
